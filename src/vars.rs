41b536077a7ca162d1dbe009d7cbfea595e93357
ryuzy_mac <onacool00700@gmail.com>
2021-11-23 20:26:54 +0900

    Mutable immutable

--- src/vars.rs
+++ src/vars.rs
@@ -1,8 +1,20 @@
 pub mod sub_a;
 pub mod sub_b;
 
+// 定数はconst 基本大文字かな？ Elixirと一緒で_つかえそう
+const MAX_POINTS: u32 = 100_000;
+
 pub fn run() {
   println!("Here is vars module!!");
-  sub_a::func_a();
-  sub_b::func_b();
+  // sub_a::func_a();
+  // sub_b::func_b();
+  // Rustはイミュータブルだけど、mutをつければ解除できる
+  let mut x = 5;
+  println!("The value of x is: {}", x);
+  x = 6;
+  println!("The value of x is: {}", x);
+
+  // 型推論　_つけると意図的に使用しないってできる
+  let _i1 = 3;
+  let _f2 = 0.1;
 }
