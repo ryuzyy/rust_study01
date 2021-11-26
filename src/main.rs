a72ccfba607bd5e3d0b8f0f5d811f2fc40dd9023
ryuzy_mac <onacool00700@gmail.com>
2021-11-26 23:05:54 +0900

    fix main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+mod ch02;
 mod stack_heep;
 // mod vars;
 
 fn main() {
     // println!("Hello, world!");
     // vars::run();
-    stack_heep::run();
+    // stack_heep::run();
     // vars::sub_a::func_a();
     // vars::sub_b::func_b();
+    ch02::run();
+    println!("Hello World!!")
 }
